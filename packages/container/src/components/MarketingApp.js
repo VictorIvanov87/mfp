60e5a6bf5e0dc241611a92dde343aafc803359fa
Victor Hristov <victor.hristov@mentormate.com>
2023-10-12 00:07:59 +0300

    Add routing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,23 @@
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { mount } from "marketing/MarketingApp";
 
 export default () => {
 	const refEl = useRef(null);
+	const history = useHistory();
 
 	useEffect(() => {
-		if (refEl) mount(refEl.current);
+		if (refEl) {
+			const { onParentNavigate } = mount(refEl.current, {
+				onNavigate: ({ pathname: nextPathname }) => {
+					if (history.location !== nextPathname) {
+						history.push(nextPathname);
+					}
+				},
+			});
+
+			history.listen(onParentNavigate);
+		}
 	}, [refEl]);
 
 	return <div ref={refEl}></div>;
